9746280613e72983d3aa6c8a5e1620979a5a1a0e
Omer Munk <93129669+OmerMunk@users.noreply.github.com>
2022-04-15 16:54:26 +0300

    Refacotred the user profile component. Added the functionality to edit profile details

--- src/Components/Users/UserProfile.js
+++ src/Components/Users/UserProfile.js
@@ -3,82 +3,75 @@ import {Button, Container} from "react-bootstrap";
 import axios from "axios";
 import AddLesson from "./addLesson";
 import {Link} from "react-router-dom";
+import EditDetailsFormModal from "./EditDetailsFormModal";
 
-const UserProfile = (props) =>{
+const UserProfile = () => {
 
     const [userProfile, setUserProfile] = useState('')
-    const [teachers, setTeachers] = useState()
-    const [students, setStudents] = useState()
+    const [credits, setCredits] = useState(0)
+    // const [teachers, setTeachers] = useState()
+    // const [students, setStudents] = useState()
     const [token, setToken] = useState('')
     const [showAddModal, setShowAddModal] = useState(false);
+    const [showEditModal, setShowEditModal] = useState(false);
 
 
     const getProfile = () => {
         const token = window.localStorage.getItem('token')
         setToken(token)
-        axios.get('http://127.0.0.1:8000/api/user/profile',{headers: {Authorization: 'Token ' + token}})
-            .then(response =>{
-                console.log(response.data.profile.type.type)
-                console.log(response.data.profile)
+        axios.get('http://127.0.0.1:8000/api/user/profile', {headers: {Authorization: 'Token ' + token}})
+            .then(response => {
                 setUserProfile(response.data.profile)
+                setCredits(response.data.credits)
             })
         console.log(userProfile)
     }
 
 
+    // const getTeachers = () => {
+    //     axios.get('http://127.0.0.1:8000/api/get_teachers', {headers: {Authorization: 'Token ' + token}})
+    //         .then(response => {
+    //             console.log(response.data)
+    //         })
+    // }
 
-    const getTeachers = () => {
-        axios.get('http://127.0.0.1:8000/api/get_teachers', {headers: {Authorization: 'Token ' + token}} )
-            .then(response =>{
-                console.log(response.data)
-            })
+    const ShowAddLessonModalHandler = () => {
+        setShowAddModal(!showAddModal);
     }
 
-    const showModal = () => {
-        setShowAddModal(!showAddModal);
+    const ShowEditModalHandler = () => {
+        setShowEditModal(!showEditModal);
     }
 
 
     useEffect(() => {
         getProfile();
-    },[])
+    }, [])
 
-    if (userProfile === ''){
-        return (
-            <h1>didnt load</h1>
-        )
-    }
-    else if (userProfile.type.type === 'student') {
-        return(
-            <Container>
-                <h1>User Profile Page</h1>
-                <p>{userProfile.user.email}</p>
-                <p>{userProfile.user.first_name}</p>
-                <p>{userProfile.user.last_name}</p>
-                <p>{userProfile.address}</p>
-                <p>{userProfile.phone_number}</p>
-                <p>{userProfile.birth_date.toString().substring(0,10)}</p>
-            </Container>
 
-        )
-    }
-    else if (userProfile.type.type === 'teacher') {
-        return(
-            <Container>
-                <h1>User Profile Page</h1>
-                <AddLesson showAdd={showAddModal} token={token} addClicked={showModal}  />
-                <p>{userProfile.user.email}</p>
-                <p>{userProfile.user.first_name}</p>
-                <p>{userProfile.user.last_name}</p>
-                <p>{userProfile.address}</p>
-                <p>{userProfile.phone_number}</p>
-                <p>{userProfile.birth_date.toString().substring(0,10)}</p>
-                <Button onClick={showModal} >Add Lesson</Button>
-                <Link to='/lessons_history'>Lessons History</Link>
-            </Container>
+    return (
+        <div>
+            {userProfile === '' && <h1>didnt load</h1>}
+            {userProfile !== '' &&
+                <Container>
+                    <h1>User Profile Page</h1>
+                    <p><strong>Email</strong>: {userProfile.user.email}</p>
+                    <p><strong>First Name:</strong> {userProfile.user.first_name}</p>
+                    <p><strong>Last Name:</strong> {userProfile.user.last_name}</p>
+                    <p><strong>Address:</strong> {userProfile.address}</p>
+                    <p><strong>Phone Number:</strong> {userProfile.phone_number}</p>
+                    <p><strong>Birth Date:</strong> {userProfile.birth_date.toString().substring(0, 10)}</p>
+                    <p><strong>Credits:</strong> {credits}</p>
+                    <Button onClick={ShowEditModalHandler}>Edit</Button><br/>
+                    <EditDetailsFormModal profile={userProfile} token={token} show={showEditModal} toggle={ShowEditModalHandler}/>
+                    <Link to='/lessons_history'>Lessons History</Link>
+                    {userProfile.type.type === 'teacher' && <div>
+                        <Button onClick={ShowAddLessonModalHandler}>Add Lesson</Button>
+                        <AddLesson showAdd={showAddModal} token={token} addClicked={ShowAddLessonModalHandler}  />
+                    </div>}
+                </Container>}
 
-        )
-    }
+        </div>
+    )
 }
-
 export default UserProfile
